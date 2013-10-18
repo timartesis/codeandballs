e776989be64397b9d965836f9ad2d7790f5002b0
Maethe <maethe.desmet@student.ap.be>
2013-10-18 16:13:56 +0200

    Opruimen code

--- Examenmonitor/Validatie.cs
+++ Examenmonitor/Validatie.cs
@@ -13,13 +13,17 @@ namespace Examenmonitor
           + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
           + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
         private static string gebrNaamPatroon = @"(?=^.{0,255}$)^[a-zA-Z][-\w.]{0,255}([a-zA-Z\d]|(?<![-.])_)$";
-        
 
-        //Reguliere expressies die het patroon van een email en gebruikersnaam sullen controleren om te zien of deze juist zijn,
+        //tussen 8 en 20 karakters, bevat minstens 1 cijfer, minstens 1 hoofdletter en minstens 1 kleine letter en geen whitespaces.
+
+        private static string wachtwoordPatroon = @"(?=^.{8,20}$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*\s).*$";
+
+        //Reguliere expressies die het patroon van een email, gebruikersnaam en wachtwoord zullen controleren om te zien of deze juist zijn,
         //controleren NIET of het adres geldig is.
 
         private static Regex JuisteMailRegex = new Regex(mailPatroon, RegexOptions.IgnoreCase);
         private static Regex JuisteGebrNaamRegex = new Regex(gebrNaamPatroon, RegexOptions.Compiled);
+        private static Regex expressieWachtwoord = new Regex(wachtwoordPatroon, RegexOptions.Compiled);
 
         public static bool Email(string mail)
         {
@@ -29,10 +33,8 @@ namespace Examenmonitor
         public static bool GebruikersNaam(string naam)
         {
             return JuisteGebrNaamRegex.IsMatch(naam);
-        //tussen 8 en 20 karakters, bevat minstens 1 cijfer, minstens 1 hoofdletter en minstens 1 kleine letter en geen whitespaces.
-        private static string regExWachtwoord = @"(?=^.{8,20}$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*\s).*$";
-        static Regex expressieWachtwoord = new Regex(regExWachtwoord, RegexOptions.Compiled);
-
+        }
+        
         public static bool CheckWachtwoord(string wachtwoord)
         {
             return expressieWachtwoord.IsMatch(wachtwoord);
