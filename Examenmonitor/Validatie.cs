e85570847cae982e51542e7c3f97b0fe544d4973
Maethe <maethe.desmet@student.ap.be>
2013-10-18 16:26:34 +0200

    opruimen code + toevoegen validatie naam

--- Examenmonitor/Validatie.cs
+++ Examenmonitor/Validatie.cs
@@ -9,35 +9,42 @@ namespace Examenmonitor
 {
     public class Validatie
     {
-        private static string mailPatroon = @"(?=^.{0,255}$)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
-          + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
-          + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
-        private static string gebrNaamPatroon = @"(?=^.{0,255}$)^[a-zA-Z][-\w.]{0,255}([a-zA-Z\d]|(?<![-.])_)$";
+        private static string mailPatroon = @"(?=^.{2,255}$)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                                            + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                                            + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+        private static string gebrNaamPatroon = @"(?=^.{2,255}$)^[a-zA-Z][-\w.]{0,255}([a-zA-Z\d]|(?<![-.])_)$";
 
         //tussen 8 en 20 karakters, bevat minstens 1 cijfer, minstens 1 hoofdletter en minstens 1 kleine letter en geen whitespaces.
 
         private static string wachtwoordPatroon = @"(?=^.{8,20}$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*\s).*$";
+        private static string naamPatroon = @"(?=^.{2,255}$)[a-zA-Z]+$";
 
-        //Reguliere expressies die het patroon van een email, gebruikersnaam en wachtwoord zullen controleren om te zien of deze juist zijn,
+        //Reguliere expressies die het patroon van een email, gebruikersnaam, wachtwoord en naam zullen controleren om te zien of deze juist zijn,
         //controleren NIET of het adres geldig is.
 
         private static Regex JuisteMailRegex = new Regex(mailPatroon, RegexOptions.IgnoreCase);
         private static Regex JuisteGebrNaamRegex = new Regex(gebrNaamPatroon, RegexOptions.Compiled);
-        private static Regex expressieWachtwoord = new Regex(wachtwoordPatroon, RegexOptions.Compiled);
+        private static Regex JuistWachtwRegex = new Regex(wachtwoordPatroon, RegexOptions.Compiled);
+        private static Regex JuisteNaamRegex = new Regex(naamPatroon, RegexOptions.Compiled);
 
-        public static bool Email(string mail)
+        public static bool ControleerEmail(string mail)
         {
             return JuisteMailRegex.IsMatch(mail);
         }
 
-        public static bool GebruikersNaam(string naam)
+        public static bool ControleerGebruikersNaam(string naam)
         {
             return JuisteGebrNaamRegex.IsMatch(naam);
         }
-        
-        public static bool CheckWachtwoord(string wachtwoord)
+
+        public static bool ControleerWachtwoord(string wachtwoord)
         {
-            return expressieWachtwoord.IsMatch(wachtwoord);
+            return JuistWachtwRegex.IsMatch(wachtwoord);
+        }
+
+        public static bool ControleerNaam(string naam)
+        {
+            return JuisteNaamRegex.IsMatch(naam);
         }
     }
 }
