218bd6eb2f441b91a5819c87e24bf5855ec2949e
Maethe <maethe.desmet@student.ap.be>
2013-10-18 14:54:49 +0200

    Email validatie toegevoegd, ook uitgetest.

--- Examenmonitor/Validatie.cs
+++ Examenmonitor/Validatie.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Examenmonitor
 {
     public class Validatie
     {
+        private static Regex JuisteMailRegex = MaakJuisteEmailRegex();
+
+        private static Regex MaakJuisteEmailRegex()
+        {
+            string mailPatroon = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(mailPatroon, RegexOptions.IgnoreCase);
+        }
+
+        public static bool Email(string mail)
+        {
+            return JuisteMailRegex.IsMatch(mail);
+        }
     }
 }
