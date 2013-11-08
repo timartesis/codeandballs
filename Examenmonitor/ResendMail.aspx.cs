1915d6bdcf072fcee8737424424c3215a5a6c899
Maethe <maethe.desmet@student.ap.be>
2013-11-08 15:23:17 +0100

    Maken van nieuwe methodes voor ophalen van volledige naam uit databank en methode voor controleren op activatie van mail,
    maken van de resendmail (moet nog aangepast woorden, zit exception in)

--- Examenmonitor/ResendMail.aspx.cs
+++ Examenmonitor/ResendMail.aspx.cs
@@ -9,12 +9,32 @@ namespace Examenmonitor
 {
     public partial class ResendMail : System.Web.UI.Page
     {
+        private MailVersturingen registratieMail;
         protected void Page_Load(object sender, EventArgs e)
         {
+            registratieMail = MailVersturingen.getInstance();
         }
 
         protected void buttonResendMail_Click(object sender, EventArgs e)
         {
+            if (Page.IsValid)
+            {
+                //Controle op mail aanwezigheid + geen activatie in databank
+                string email = Email.Text;
+
+                if (!DatabankConnector.ControleerBestaandeEmail(email) && !DatabankConnector.ControleerActivatieEmail(email))
+                    {
+                        //Code om values uit de form te halen
+                        string volledigeNaam = DatabankConnector.GetVoornaamEnAchternaam(email);
+
+                        registratieMail.ZendRegistratieMail(volledigeNaam, email, DatabankConnector.RegistratieMail(email));
+                        Response.Redirect("~/MailCheck.aspx");
+                    }
+                else
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
         }
     }
-}
