8cdc05d90657174271842679b6d25c6bd90b8d26
Maethe <maethe.desmet@student.ap.be>
2013-11-15 15:22:46 +0100

    Toevoegen methode om wachtwoord op te halen adhv mail, aanmaken van paginas voor passwoord manueel wijzigen

---
+++ Examenmonitor/PassWijzigen.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Examenmonitor
+{
+    public partial class PassWijzigen : System.Web.UI.Page
+    {
+        string gebruiker;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Logged"].Equals("No"))
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                gebruiker = Session["User"].ToString();
+            }
+
+        }
+
+        protected void buttonWijzig_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string oudPass = OudPasswoord.Text;
+                string wachtwoord = ConfirmPassword.Text;
+                if (oudPass.Equals(wachtwoord))
+                {
+                    incorrectLabel.Text = "Het gegeven oud wachtwoord is identiek aan het nieuwe wachtwoord!";
+                    incorrectLabel.Visible = true;
+                    OudPasswoord.Text = "";
+                    Password.Text = "";
+                    ConfirmPassword.Text = "";
+                }
+                else
+                {
+                    bool controlePasswoord = DatabankConnector.vergelijkPasswoorden(DatabankConnector.GetPasswoordMetMail(gebruiker), oudPass);
+
+                    if (controlePasswoord)
+                    {
+                        DatabankConnector.changePassword(gebruiker, wachtwoord);
+                        Response.Redirect("PassWijzigenVoltooid.aspx");
+                    }
+                    else
+                    {
+                        incorrectLabel.Text = "Het gegeven oud wachtwoord is incorrect!";
+                        incorrectLabel.Visible = true;
+                        OudPasswoord.Text = "";
+                        Password.Text = "";
+                        ConfirmPassword.Text = "";
+                    }
+                }
+                
+                
+            }
+
+        }
+    }
+}
