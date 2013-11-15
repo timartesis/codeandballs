51a3d1136aa3725eedd1fd23358e3978418dc150
timartesis <tim.goeyvaerts@student.artesis.be>
2013-11-15 16:00:37 +0100

    Full capacity filter toegevoegd

---
+++ Examenmonitor/FilterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examenmonitor
+{
+    public static class FilterModel
+    {
+        public static List<Examen> filterExamensFullCapacity(List<Examen> original) {
+            List<Examen> result = new List<Examen>();
+
+            foreach (Examen ex in original)
+            {
+                if (ex.Reservaties.Count < ex.Capaciteit)
+                {
+                    result.Add(ex);
+                }
+            }
+            return result;
+        }
+    }
+}
