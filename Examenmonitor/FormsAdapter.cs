75d94de72b2223c6b7d33c454c68ebfb46ceb411
timartesis <tim.goeyvaerts@student.artesis.be>
2013-11-22 14:38:29 +0100

    fix postback

---
+++ Examenmonitor/FormsAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.Adapters;
+
+namespace Examenmonitor
+{
+    public class FormControlAdapter : ControlAdapter
+    {
+        protected override void Render(System.Web.UI.HtmlTextWriter writer)
+        {
+            base.Render(new RewriteFormHtmlTextWriter(writer));
+        }
+
+        public class RewriteFormHtmlTextWriter : HtmlTextWriter
+        {
+            public RewriteFormHtmlTextWriter(HtmlTextWriter writer)
+                : base(writer)
+            {
+                this.InnerWriter = writer.InnerWriter;
+            }
+
+            public override void WriteAttribute(string name, string value,
+                                                bool fEncode)
+            {
+                if (name.Equals("action") && string.IsNullOrEmpty(value))
+                {
+                    value = "default.aspx";
+                }
+                base.WriteAttribute(name, value, fEncode);
+            }
+        }
+    }
+}
